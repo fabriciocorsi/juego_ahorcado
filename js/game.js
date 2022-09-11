3702294d6dc2afcf3682c5d33cac6a3292953e7c
fabriciocorsi <110356898+fabriciocorsi@users.noreply.github.com>
2022-09-11 15:12:34 -0300

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,11 @@
+document.querySelector(".btn-nuevo-juego").addEventListener("click", function (e) {
+    window.location.href = "./game.html";
+});
+
+document.querySelector(".bi-github").addEventListener("click", function (e) {
+    window.open("https://github.com/fabriciocorsi", "_blank");
+});
+
+document.querySelector(".bi-linkedin").addEventListener("click", function (e) {
+    window.open("https://www.linkedin.com/in/fabricio-corsi-a18411247/", "_blank");
+});
